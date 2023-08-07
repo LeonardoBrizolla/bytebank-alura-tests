f22b19f2314c839279e5bfa2dfe130601abe7763
Leonardo Brizolla <leonardobrizollars@gmail.com>
2023-08-07 19:28:25 -0300

    Testando um clique + desafio (select/option)

--- src/componentes/Transacao/Formulario/Formulario.test.js
+++ src/componentes/Transacao/Formulario/Formulario.test.js
@@ -21,4 +21,36 @@ describe('Deve renderizar um campo de input', () => {
     userEvent.type(campoTexto, '50');
     expect(campoTexto).toHaveValue(50);
   });
+
+  test('Deve chamar um evento de onSubmit ao clicar em realizar transação', () => {
+    const realizarTransacao = jest.fn();
+
+    render(<Formulario realizarTransacao={realizarTransacao} />);
+    const button = screen.getByRole('button');
+
+    userEvent.click(button);
+    expect(realizarTransacao).toHaveBeenCalledTimes(1);
+  });
+
+  test('Deve selecionar uma opção de transação', () => {
+    render(<Formulario />);
+
+    const select = screen.getByRole('combobox');
+
+    userEvent.selectOptions(select, ['Depósito']);
+
+    const optionDefault = screen.getByRole('option', {
+      name: 'Selecione um tipo de transação',
+    }).selected;
+    const optionDeposito = screen.getByRole('option', {
+      name: 'Depósito',
+    }).selected;
+    const optionTransferencia = screen.getByRole('option', {
+      name: 'Transferência',
+    }).selected;
+
+    expect(optionDefault).toBe(false);
+    expect(optionDeposito).toBe(true);
+    expect(optionTransferencia).toBe(false);
+  });
 });
